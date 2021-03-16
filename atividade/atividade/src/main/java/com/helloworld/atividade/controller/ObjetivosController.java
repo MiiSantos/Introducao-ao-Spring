5629d8ba38a3f435dccdc1817d1f7ff8546c7147
Miria Santos <miriaandressa1@gmail.com>
2021-03-16 19:37:40 -0300

    Introdução ao Spring

--- atividade/atividade/src/main/java/com/helloworld/atividade/controller/ObjetivosController.java
+++ atividade/atividade/src/main/java/com/helloworld/atividade/controller/ObjetivosController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/frase")
-public class AtividadeController {
+@RequestMapping("/objetivo")
+public class ObjetivosController {
 
 	@GetMapping 
-	public String frase() {
-		return "Persistência e Orientação ao Detalhe!!";
+	public String objetivo() {
+		return "Objetivos, aprender Spring!!";
 	}
 }
